1552fe9d6c390dd266755efb96241f4ab77bfaad
Gurdyal006 <gurdyal.singh@tmotions.com>
2024-02-20 11:04:42 +0530

    handle-error

---
+++ app/meals/error.js
@@ -0,0 +1,9 @@
+"use client";
+export default function error({ error }) {
+  return (
+    <main className="error">
+      <h1>An error occured!....</h1>
+      <p>Failed to fetch meal details....</p>
+    </main>
+  );
+}
